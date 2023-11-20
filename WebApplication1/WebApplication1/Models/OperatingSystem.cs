09e48c6f1015d15352d484c7d5f4a3c65638ee1e
Isidora Brasancevic <isidorabr@gmail.com>
2023-11-20 20:17:08 +0100

    Added new model Operating System

---
+++ WebApplication1/WebApplication1/Models/OperatingSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OperatingSystem
+    {
+        public int OperatingSystemId { get; set; }
+        public string Description { get; set; }
+    }
+}
