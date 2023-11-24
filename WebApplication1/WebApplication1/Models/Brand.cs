1f248174b4e370afd25b12c31636fed7c38392a6
Isidora Brasancevic <isidorabr@gmail.com>
2023-11-24 11:16:36 +0100

    ModelOpratingSystem added

--- WebApplication1/WebApplication1/Models/Brand.cs
+++ WebApplication1/WebApplication1/Models/Brand.cs
@@ -8,6 +8,6 @@ namespace WebApplication1.Models
     public class Brand
     {
         public int BrandId { get; set; }
-        public string BrandName { get; set; }
+        public string Text { get; set; }
     }
 }
